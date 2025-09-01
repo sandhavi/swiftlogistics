63672f09d8802c95070ffe018c4bf89e2bbaf52a
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2025-09-01 09:01:24 +0530

    feat: enhance SignUpPage typography by applying Poppins and Lora fonts

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -125,7 +125,7 @@ export default function SignUpPage() {
         }
     };
     return (
-        <div className="min-h-screen bg-white flex font-para text-gray-900">
+        <div className="min-h-screen bg-white flex font-para text-gray-900 font-poppins tracking-wider">
 
             {/* Right Side - Registration Form */}
             <div className="flex-1 flex items-center justify-center px-6 py-12">
@@ -141,7 +141,7 @@ export default function SignUpPage() {
                     </div>
 
                     <div className="text-center mb-8">
-                        <h2 className="text-3xl font-bold text-gray-900 mb-2">Create Account</h2>
+                        <h2 className="text-3xl font-bold text-gray-900 mb-2 font-lora">Create Account</h2>
                         <p className="text-gray-600">Get started for track your order</p>
                     </div>
 
